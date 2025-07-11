2487e4ee9ba7ffd1ae9f5b5c44544e7dd86e4330
Gorong22 <mare454545@gmail.com>
2025-07-11 17:53:13 +0900

    V2_250711_won_kakao

---
+++ API_Example/kakao_map_project/script.js
@@ -0,0 +1,9 @@
+const url =
+  "https://apis.data.go.kr/B551011/GoCamping/basedList?numOfRows=4000&pageNo=1&MobileOS=ETC&MobileApp=camnavi&serviceKey=vXpamPPqXWsORdlj04YF6P%2FII0iMXCy2cV5WrUe78yyyLIH3JSQ5Fl82BQb0YzAJwwhceO%2Bxn%2Fae1OvcA7pXmg%3D%3D&_type=json";
+
+fetch(url)
+  .then((response) => response.json())
+  .then((result) => {
+    const data = result.response.body.items.item;
+    console.log(data);
+  });
