7b299ecbc0788e7fe064642bc3e0644e8d836be2
Gorong22 <mare454545@gmail.com>
2025-08-11 19:00:03 +0900

    250811_11번가, 캠핑용품 검색완료, 접속후 토글 클릭, 인기순 정렬 클릭까지 성공, 그러나 상품명을 못긁어옴.. 이상한거 긁어옴

---
+++ SQL/250808/won_sql6_musinsa_db_v5.sql
@@ -0,0 +1,19 @@
+#use musinsa_db_v4
+
+use musinsa_db_v5;
+
+select * from reviews limit 1;
+
+
+
+# 크롤링한 데이터를 기반으로 상품에 대한 충성도 및 어뷰징 검증
+select 
+	상품명, 
+    avg(char_length(리뷰)) 평균_리뷰_길이
+from reviews
+group by 상품명
+order by 평균_리뷰_길이 desc;
+
+
+select count(*) from reviews
+where 리뷰 like '%별로%' or 리뷰 like '%불편%';
