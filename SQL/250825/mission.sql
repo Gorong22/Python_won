d7ca0940bb822b5983ba3a4375ea3f0bfb954db2
Gorong22 <mare454545@gmail.com>
2025-09-21 12:37:09 +0900

    250921

--- SQL/250825/mission.sql
+++ SQL/250825/mission.sql
@@ -16,3 +16,21 @@ create table booklist(
 
 
 select * from booklist;
+
+create database if not exists playground;
+
+use playground;
+
+create table books (
+	id int auto_increment primary key,
+    title varchar(100) not null,
+    price_gdp int not null,
+    rating float not null,
+    review_count int not null,
+    pr_url varchar(200) not null
+
+
+);
+
+select * from books 
+limit 10;
